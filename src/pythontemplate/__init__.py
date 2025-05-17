830d1453b81b176781ea0832b7a89a839ee33831
narumi <toucans-cutouts0f@icloud.com>
2025-05-17 18:27:19 +0800

    ✨ refactor(logging): simplify logger configuration by removing RichHandler

--- src/pythontemplate/__init__.py
+++ src/pythontemplate/__init__.py
@@ -1,16 +1,8 @@
 import os
+import sys
 from typing import Final
 
 from loguru import logger
-from rich.logging import RichHandler
 
 LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO")
-logger.configure(
-    handlers=[
-        {
-            "sink": RichHandler(show_time=False, omit_repeated_times=False, show_level=False, show_path=False),
-            "level": LOGURU_LEVEL,
-            # "format": "{time:YYYY-MM-DD HH:mm:ss,SSS} {level: <8} '{name}:{function}:{line}' {message}",
-        },
-    ]
-)
+logger.configure(handlers=[{"sink": sys.stderr, "level": LOGURU_LEVEL}])
