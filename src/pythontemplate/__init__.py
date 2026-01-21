a24aa5eff663cd9a1a0601dd97fee80da93f1f7a
narumi <toucans-cutouts0f@icloud.com>
2026-01-22 03:14:10 +0800

    Remove loguru dependency and related logging setup

--- src/pythontemplate/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from typing import Final
-
-from loguru import logger
-
-LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO")
-logger.remove()
-logger.add(sys.stderr, level=LOGURU_LEVEL)
--- src/pythontemplate/cli.py
+++ src/pythontemplate/cli.py
@@ -1,9 +1,15 @@
+import logging
+
 import typer
-from loguru import logger
+
+logger = logging.getLogger(__name__)
+
 
 app = typer.Typer()
 
 
 @app.command()
 def main() -> None:
+    format_str = "%(asctime)s | %(levelname)s | %(name)s:%(lineno)d - %(message)s"
+    logging.basicConfig(format=format_str, level=logging.INFO)
     logger.info("Hello, world!")
